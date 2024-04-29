4e685d9e67886c22bb86c33499770c363f44653f
JavierTT-03 <javiertomas2003@gmail.com>
2024-04-29 21:33:36 +0200

    29-04-2024_21:30
    FrameWork Inicido, View ControllerContact

---
+++ Module/Contact/Controller/ControllerContact.class.php
@@ -0,0 +1,25 @@
+<?php
+	class ControllerContact {
+		
+		function View(){
+			echo 'View ControllerContact Está vivo';
+			//common::load_view('top_page_contact.html', VIEW_PATH_CONTACT . 'contact_list.html');
+		}
+		
+		// function send_contact_us(){
+		// 	$message = ['type' => 'contact',
+		// 				'inputName' => $_POST['name'], 
+		// 				'fromEmail' => $_POST['email'], 
+		// 				'inputMatter' => $_POST['matter'], 
+		// 				'inputMessage' => $_POST['message']];
+		// 	$email = json_decode(mail::send_email($message), true);
+			
+		// 	if (!empty($email)) {
+		// 		echo json_encode('Done!');
+		// 		return;  
+		// 	} else {
+		// 		echo json_encode('Error!');
+		// 	}
+		// }
+	}
+?>
