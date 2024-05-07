cc6d76199263c21d10df631b1570455495654a6b
JavierTT-03 <javiertomas2003@gmail.com>
2024-05-07 17:49:37 +0200

    Conexión a la Base de Datos

---
+++ module/home/model/BLL/home_bll.class.singleton.php
@@ -0,0 +1,31 @@
+<?php
+	class home_bll {
+		private $dao;
+		private $db;
+		static $_instance;
+
+		function __construct() {
+			$this -> dao = home_dao::getInstance();
+			$this -> db = db::getInstance();
+		}
+
+		public static function getInstance() {
+			if (!(self::$_instance instanceof self)) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+
+		public function get_carrusel_BLL() {
+			return $this -> dao -> select_data_carrusel($this -> db);
+		}
+
+		public function get_category_BLL() {
+			return $this -> dao -> select_data_category($this -> db);
+		}
+
+		public function get_type_BLL() {
+			return $this -> dao -> select_data_type($this -> db);
+		}
+	}
+?>
