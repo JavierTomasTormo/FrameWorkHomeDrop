67ff1f70ce8d8b02a5009344b538e2b044b6163e
JavierTT-03 <javiertomas2003@gmail.com>
2024-05-13 21:35:58 +0200

    Autoload Adapted

--- autoload.php
+++ autoload.php
@@ -16,14 +16,18 @@
         if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
             set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
             spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.singleton.php')){
+
+        } else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.singleton.php')){
             set_include_path(SITE_ROOT . 'model/');
             spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.php')){
+
+        } else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.php')){
             set_include_path(SITE_ROOT . 'model/');
             spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'utils/' . $className . '.inc.php')) {
+
+        } else if (file_exists(SITE_ROOT . 'utils/' . $className . '.inc.php')) {
             set_include_path(SITE_ROOT . 'utils/');
             spl_autoload($className);
+            
         }
     }
