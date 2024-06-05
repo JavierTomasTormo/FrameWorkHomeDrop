390513ed6eb6848b40073856361032e9340b030e
JavierTomasTormo <javiertomas2003@gmail.com>
2024-06-05 21:48:19 +0200

    Trash, Less, More & details

--- module/cart/view/js/controller_cart.js
+++ module/cart/view/js/controller_cart.js
@@ -53,17 +53,19 @@ function ListCart() {
                                 <div class="cart-item-image">
                                     <img src="${matchedItem.Img}" alt="Imagen del producto">
                                     <p><strong>Visitas:</strong> ${matchedItem.vivistas}</p>
-
+                                    
                                 </div>
+                                
                                 <div class="cart-item-general">
                                     <p><strong>Categoría:</strong> ${matchedItem.Category}</p>
                                     <p><strong>Operación:</strong> ${matchedItem.Operation}</p>
                                     <p><strong>Tipo:</strong> ${matchedItem.Type}</p>
                                 </div>
-                                <div class="cart-item-details">
+                                <div class="cart-item-details">                                 
                                     <p><strong>Ciudad:</strong> ${matchedItem.Ciudad}</p>
                                     <p><strong>Calle:</strong> ${matchedItem.Calle}</p>
                                     <p><strong>Superficie:</strong> ${matchedItem.Superficie} m²</p>
+                                    
                                 </div>
                                 <div class="cart-item-price-container">
 
@@ -79,6 +81,24 @@ function ListCart() {
                                         <p><strong>Total:</strong><br>${itemTotalPrice.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}€</p>
                                     </div>
                                 </div>
+                                <div class="cart-item-remove">
+                                    <p class="remove-item" id="${cartItem.ID_HomeDrop}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                            <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
+                                        </svg>
+                                    </p>
+                                </div>
+                                <div class="Button-min" id="${cartItem.ID_HomeDrop}">
+                                    <p class="minus-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ff1a1a" d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM152 232H296c13.3 0 24 10.7 24 24s-10.7 24-24 24H152c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
+                                    </p>
+                                </div>
+                                <div class="Button-max" id="${cartItem.ID_HomeDrop}">
+                                    <p class="plus-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#1aff47" d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                    </p>
+                                </div>
+
                         `;
                         // console.log(matchedItem);
                 
@@ -108,16 +128,43 @@ function ListCart() {
 }
 //#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·//
 //#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·//ProcreedToPay
-function ButtonProcreedToPay() {
-    $(document).on('click', '#ProcreedToPay', function () {
-        console.log("ProcreedToPay");
-    });
+function Buttons() {
+    /*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+        $(document).on('click', '#ProcreedToPay', function () {
+            console.log("ProcreedToPay");
+
+        });
+    /*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+        $(document).on('click', '.remove-item', function () {
+            var ID_HomeDrop = this.id.replace('remove-item-', '');
+
+            console.log(ID_HomeDrop);
+
+            console.log("remove");
+        });
+    /*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+        $(document).on('click', '.Button-min', function () {
+            var ID_HomeDrop = this.id.replace('Button-min-', '');
+
+            console.log(ID_HomeDrop);
+
+            console.log("Menos");
+        });
+    /*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
+        $(document).on('click', '.Button-max', function () {
+            var ID_HomeDrop = this.id.replace('Button-max-', '');
+
+            console.log(ID_HomeDrop);
+
+            console.log("Mas");
+        });
+    /*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*/
 }
 //#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·//
 //#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·#·//
 $(document).ready(function () {
     ListCart();
-    ButtonProcreedToPay();
-    // console.info("ready CART");
+    Buttons();
+    // console.info("ready CART");remove-item
 
 });
